60f3ab5ba5f092402a415fd197db323514c28d86
Matt Pusch <mpusch88@gmail.com>
2023-04-14 15:12:25 -0700

    added wpf project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace mnemonicr
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string input = InputTextBox.Text.Trim();
+
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "powershell.exe",
+                    Arguments = $"-ExecutionPolicy Bypass -File script.ps1 -InputString \"{input}\"",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            List<string> mnemonics = new List<string>(output.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries));
+
+            ResultListBox.ItemsSource = mnemonics;
+        }
+
+        private void CopyButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ResultListBox.SelectedItems.Count > 0)
+            {
+                string copyText = string.Join(Environment.NewLine, ResultListBox.SelectedItems);
+                Clipboard.SetText(copyText);
+            }
+        }
+    }
+}
--- script.ps1
+++ script.ps1
@@ -1,8 +1,27 @@
-﻿$mnems = ""
+param(
+    [string]$InputString
+)
 
-foreach($name in $args)
-{
-	$mnems += get-mailbox $name | select samaccountname
+function IsEmail($input) {
+    return $input -match "^[\w\.-]+@[\w\.-]+\.\w+$"
 }
 
-returns $mnems
+$entries = $InputString -split ";"
+$mnems = @()
+
+foreach ($entry in $entries) {
+    $entry = $entry.Trim()
+
+    if (IsEmail $entry) {
+        $mnems += Get-ADUser -Filter "EmailAddress -eq '$entry'" -Properties EmailAddress, SamAccountName | Select-Object SamAccountName
+    } else {
+        $nameParts = $entry -split ","
+        if ($nameParts.Length -eq 2) {
+            $firstName = $nameParts[0].Trim()
+            $lastName = $nameParts[1].Trim()
+            $mnems += Get-ADUser -Filter "GivenName -eq '$firstName' -and Surname -eq '$lastName'" -Properties GivenName, Surname, SamAccountName | Select-Object SamAccountName
+        }
+    }
+}
+
+$mnems | ForEach-Object { $_.SamAccountName }
